9d112510036f8f4c33295d7275315d553aa5ccde
Zone.N <zone.niuzh@hotmail.com>
2024-12-06 08:37:34 +0000

    test pre-commit
    Signed-off-by: Zone.N <zone.niuzh@hotmail.com>

--- cmake/functions.cmake
+++ cmake/functions.cmake
@@ -1,4 +1,3 @@
-
 # This file is a part of Simple-XX/SimpleKernel
 # (https://github.com/Simple-XX/SimpleKernel).
 #
@@ -6,91 +5,83 @@
 # 辅助函数
 
 # 生成 target 输出文件的 objdump -D, readelf -a, nm -a
-# _target: target 名
-# 在 ${${_target}_BINARY_DIR} 目录下生成 $<TARGET_FILE:${_target}>.asm 文件
-# 在 ${${_target}_BINARY_DIR} 目录下生成 $<TARGET_FILE:${_target}>.readelf 文件
-# 在 ${${_target}_BINARY_DIR} 目录下生成 $<TARGET_FILE:${_target}>.sym 文件
-function(objdump_readelf_nm _target)
-    add_custom_command(TARGET ${_target}
-            VERBATIM
-            POST_BUILD
-            DEPENDS ${_target}
-            WORKING_DIRECTORY ${${_target}_BINARY_DIR}
-            COMMAND ${CMAKE_OBJDUMP} -D $<TARGET_FILE:${_target}> > $<TARGET_FILE_DIR:${_target}>/${_target}.asm || exit 0
-            COMMAND ${CMAKE_READELF} -a $<TARGET_FILE:${_target}> > $<TARGET_FILE_DIR:${_target}>/${_target}.readelf || exit 0
-            COMMAND ${CMAKE_NM} -a $<TARGET_FILE:${_target}> > $<TARGET_FILE_DIR:${_target}>/${_target}.sym
-            COMMENT "Generating symbol table, assembly, and readelf result for ${_target}"
-    )
-    set_directory_properties(PROPERTIES ADDITIONAL_MAKE_CLEAN_FILES
-            "$<TARGET_FILE_DIR:${_target}>/${_target}.asm;$<TARGET_FILE_DIR:${_target}>/${_target}.readelf;$<TARGET_FILE_DIR:${_target}>/${_target}.sym;"
-    )
-endfunction()
+# target: target 名
+# 在 ${${target}_BINARY_DIR} 目录下生成 $<TARGET_FILE:${target}>.asm 文件
+# 在 ${${target}_BINARY_DIR} 目录下生成 $<TARGET_FILE:${target}>.readelf 文件
+# 在 ${${target}_BINARY_DIR} 目录下生成 $<TARGET_FILE:${target}>.sym 文件
+FUNCTION(objdump_readelf_nm target)
+    ADD_CUSTOM_COMMAND (
+        TARGET ${target}
+        VERBATIM POST_BUILD DEPENDS ${target}
+        WORKING_DIRECTORY ${${target}_BINARY_DIR}
+        COMMAND ${CMAKE_OBJDUMP} -D $<TARGET_FILE:${target}> >
+                $<TARGET_FILE_DIR:${target}>/${target}.asm || exit 0
+        COMMAND ${CMAKE_READELF} -a $<TARGET_FILE:${target}> >
+                $<TARGET_FILE_DIR:${target}>/${target}.readelf || exit 0
+        COMMAND ${CMAKE_NM} -a $<TARGET_FILE:${target}> >
+                $<TARGET_FILE_DIR:${target}>/${target}.sym
+        COMMENT "Generating symbol table, assembly, and readelf for ${target}")
+    SET_DIRECTORY_PROPERTIES (
+        PROPERTIES ADDITIONAL_MAKE_CLEAN_FILES
+                   "$<TARGET_FILE_DIR:${target}>/${target}.asm;"
+                   "$<TARGET_FILE_DIR:${target}>/${target}.readelf;"
+                   "$<TARGET_FILE_DIR:${target}>/${target}.sym;")
+ENDFUNCTION()
 
 # 将 elf 转换为 efi
-# _elf: 要转换的 target 名
-# _efi: 输出的 efi 文件名
-# 在 ${${_target}_BINARY_DIR} 目录下生成 ${_efi} 文件
-function(elf2efi _target _efi)
-    add_custom_command(TARGET ${_target}
-            COMMENT "Convert $<TARGET_FILE:${_target}> to efi ..."
-            POST_BUILD
-            DEPENDS ${_target}
-            WORKING_DIRECTORY ${${_target}_BINARY_DIR}
-            COMMAND ${CMAKE_OBJCOPY} $<TARGET_FILE:${_target}> ${_efi}
-            -S
-            -R .comment
-            -R .note.gnu.build-id
-            -R .gnu.hash
-            -R .dynsym
-            --target=efi-app-${CMAKE_SYSTEM_PROCESSOR} --subsystem=10
-    )
-endfunction()
+# efi: 输出的 efi 文件名
+# 在 ${${target}_BINARY_DIR} 目录下生成 ${efi} 文件
+FUNCTION(elf2efi target efi)
+    ADD_CUSTOM_COMMAND (
+        TARGET ${target}
+        COMMENT "Convert $<TARGET_FILE:${target}> to efi ..."
+        POST_BUILD DEPENDS ${target}
+        WORKING_DIRECTORY ${${target}_BINARY_DIR}
+        COMMAND
+            ${CMAKE_OBJCOPY} $<TARGET_FILE:${target}> ${efi} -S -R .comment -R
+            .note.gnu.build-id -R .gnu.hash -R .dynsym
+            --target=efi-app-${CMAKE_SYSTEM_PROCESSOR} --subsystem=10)
+ENDFUNCTION()
 
 # 添加测试覆盖率 target
 # DEPENDS 要生成的 targets
 # SOURCE_DIR 源码路径
 # BINARY_DIR 二进制文件路径
 # EXCLUDE_DIR 要排除的目录
-function(add_coverage_target)
+FUNCTION(add_coverage_target)
     # 解析参数
-    set(options)
-    set(one_value_keywords SOURCE_DIR BINARY_DIR)
-    set(multi_value_keywords DEPENDS EXCLUDE_DIR)
-    cmake_parse_arguments(
-            ARG "${options}" "${one_value_keywords}" "${multi_value_keywords}" ${ARGN}
-    )
+    SET (options)
+    SET (one_value_keywords SOURCE_DIR BINARY_DIR)
+    SET (multi_value_keywords DEPENDS EXCLUDE_DIR)
+    CMAKE_PARSE_ARGUMENTS (ARG "${options}" "${one_value_keywords}"
+                           "${multi_value_keywords}" ${ARGN})
 
     # 不检查的目录
-    list(APPEND EXCLUDES --exclude)
-    foreach (_item ${ARG_EXCLUDE_DIR})
-        list(APPEND EXCLUDES '${_item}')
-    endforeach ()
+    LIST (APPEND EXCLUDES --exclude)
+    FOREACH(item ${ARG_EXCLUDE_DIR})
+        LIST (APPEND EXCLUDES '${item}')
+    ENDFOREACH()
 
     # 添加 target
-    add_custom_target(coverage DEPENDS ${ARG_DEPENDS}
-            COMMAND ${CMAKE_CTEST_COMMAND}
-    )
+    ADD_CUSTOM_TARGET (
+        coverage
+        COMMENT ""
+        DEPENDS ${ARG_DEPENDS}
+        COMMAND ${CMAKE_CTEST_COMMAND})
     # 在 coverage 执行完毕后生成报告
-    add_custom_command(TARGET coverage
-            COMMENT "Generating coverage report ..."
-            POST_BUILD
-            WORKING_DIRECTORY ${ARG_BINARY_DIR}
-            COMMAND ${CMAKE_COMMAND} -E make_directory ${COVERAGE_OUTPUT_DIR}
-            COMMAND ${LCOV_EXE}
-            -c
-            -o ${COVERAGE_OUTPUT_DIR}/coverage.info
-            -d ${ARG_BINARY_DIR}
-            -b ${ARG_SOURCE_DIR}
-            --no-external
-            ${EXCLUDES}
-            --rc branch_coverage=1
-            --ignore-errors mismatch
-            COMMAND ${GENHTML_EXE}
-            ${COVERAGE_OUTPUT_DIR}/coverage.info
-            -o ${COVERAGE_OUTPUT_DIR}
-            --branch-coverage
-    )
-endfunction()
+    ADD_CUSTOM_COMMAND (
+        TARGET coverage
+        COMMENT "Generating coverage report ..."
+        POST_BUILD
+        WORKING_DIRECTORY ${ARG_BINARY_DIR}
+        COMMAND ${CMAKE_COMMAND} -E make_directory ${COVERAGE_OUTPUT_DIR}
+        COMMAND
+            ${LCOV_EXE} -c -o ${COVERAGE_OUTPUT_DIR}/coverage.info -d
+            ${ARG_BINARY_DIR} -b ${ARG_SOURCE_DIR} --no-external ${EXCLUDES}
+            --rc branch_coverage=1 --ignore-errors mismatch
+        COMMAND ${GENHTML_EXE} ${COVERAGE_OUTPUT_DIR}/coverage.info -o
+                ${COVERAGE_OUTPUT_DIR} --branch-coverage)
+ENDFUNCTION()
 
 # 添加运行 qemu target
 # NAME 生成的 target 前缀
@@ -100,57 +91,82 @@ endfunction()
 # KERNEL kernel 文件路径
 # DEPENDS 依赖的 target
 # QEMU_FLAGS qemu 参数
-function(add_run_target)
+FUNCTION(add_run_target)
     # 解析参数
-    set(options)
-    set(one_value_keywords NAME TARGET WORKING_DIRECTORY BOOT KERNEL)
-    set(multi_value_keywords DEPENDS QEMU_FLAGS)
-    cmake_parse_arguments(
-            ARG "${options}" "${one_value_keywords}" "${multi_value_keywords}" ${ARGN}
-    )
+    SET (options)
+    SET (one_value_keywords NAME TARGET WORKING_DIRECTORY BOOT KERNEL)
+    SET (multi_value_keywords DEPENDS QEMU_FLAGS)
+    CMAKE_PARSE_ARGUMENTS (ARG "${options}" "${one_value_keywords}"
+                           "${multi_value_keywords}" ${ARGN})
 
-    list(APPEND commands
-            COMMAND ${CMAKE_COMMAND} -E copy ${ARG_KERNEL} image/
-    )
-    if (${ARG_TARGET} STREQUAL "x86_64")
-        get_filename_component(BOOT_FILE_NAME ${ARG_BOOT} NAME)
-        configure_file(${CMAKE_SOURCE_DIR}/tools/startup.nsh.in image/startup.nsh @ONLY)
-        list(APPEND commands
-                COMMAND ${CMAKE_COMMAND} -E copy ${ARG_BOOT} image/
-        )
-    elseif (${ARG_TARGET} STREQUAL "riscv64")
-        get_filename_component(BOOT_FILE_NAME ${ARG_BOOT} NAME)
-        configure_file(${CMAKE_SOURCE_DIR}/tools/startup.nsh.in image/startup.nsh @ONLY)
-        list(APPEND commands
-                COMMAND ${CMAKE_COMMAND} -E copy ${ARG_BOOT} image/
-        )
-    elseif (${ARG_TARGET} STREQUAL "aarch64")
-        get_filename_component(BOOT_FILE_NAME ${ARG_BOOT} NAME)
-        configure_file(${CMAKE_SOURCE_DIR}/tools/startup.nsh.in image/startup.nsh @ONLY)
-        list(APPEND commands
-                COMMAND ${CMAKE_COMMAND} -E copy ${ARG_BOOT} image/
-        )
-    endif ()
+    LIST (
+        APPEND
+        commands
+        COMMAND
+        ${CMAKE_COMMAND}
+        -E
+        copy
+        ${ARG_KERNEL}
+        image/)
+    IF(${ARG_TARGET} STREQUAL "x86_64")
+        GET_FILENAME_COMPONENT (BOOT_FILE_NAME ${ARG_BOOT} NAME)
+        CONFIGURE_FILE (${CMAKE_SOURCE_DIR}/tools/startup.nsh.in
+                        image/startup.nsh @ONLY)
+        LIST (
+            APPEND
+            commands
+            COMMAND
+            ${CMAKE_COMMAND}
+            -E
+            copy
+            ${ARG_BOOT}
+            image/)
+    ELSEIF(${ARG_TARGET} STREQUAL "riscv64")
+        GET_FILENAME_COMPONENT (BOOT_FILE_NAME ${ARG_BOOT} NAME)
+        CONFIGURE_FILE (${CMAKE_SOURCE_DIR}/tools/startup.nsh.in
+                        image/startup.nsh @ONLY)
+        LIST (
+            APPEND
+            commands
+            COMMAND
+            ${CMAKE_COMMAND}
+            -E
+            copy
+            ${ARG_BOOT}
+            image/)
+    ELSEIF(${ARG_TARGET} STREQUAL "aarch64")
+        GET_FILENAME_COMPONENT (BOOT_FILE_NAME ${ARG_BOOT} NAME)
+        CONFIGURE_FILE (${CMAKE_SOURCE_DIR}/tools/startup.nsh.in
+                        image/startup.nsh @ONLY)
+        LIST (
+            APPEND
+            commands
+            COMMAND
+            ${CMAKE_COMMAND}
+            -E
+            copy
+            ${ARG_BOOT}
+            image/)
+    ENDIF()
 
     # 添加 target
-    add_custom_target(${ARG_NAME}run DEPENDS ${ARG_DEPENDS}
-            WORKING_DIRECTORY ${ARG_WORKING_DIRECTORY}
-            COMMAND ${CMAKE_COMMAND} -E make_directory image/
-            ${commands}
-            COMMAND
-            qemu-system-${ARG_TARGET}
-            ${ARG_QEMU_FLAGS}
-    )
-    add_custom_target(${ARG_NAME}debug DEPENDS ${ARG_DEPENDS}
-            WORKING_DIRECTORY ${ARG_WORKING_DIRECTORY}
-            COMMAND ${CMAKE_COMMAND} -E make_directory image/
-            ${commands}
-            COMMAND
-            qemu-system-${ARG_TARGET}
-            ${ARG_QEMU_FLAGS}
+    ADD_CUSTOM_TARGET (
+        ${ARG_NAME}run
+        COMMENT "Run ${ARG_NAME} ..."
+        DEPENDS ${ARG_DEPENDS}
+        WORKING_DIRECTORY ${ARG_WORKING_DIRECTORY}
+        COMMAND ${CMAKE_COMMAND} -E make_directory image/ ${commands}
+        COMMAND qemu-system-${ARG_TARGET} ${ARG_QEMU_FLAGS})
+    ADD_CUSTOM_TARGET (
+        ${ARG_NAME}debug
+        COMMENT "Run ${ARG_NAME} ..."
+        DEPENDS ${ARG_DEPENDS}
+        WORKING_DIRECTORY ${ARG_WORKING_DIRECTORY}
+        COMMAND ${CMAKE_COMMAND} -E make_directory image/ ${commands}
+        COMMAND
+            qemu-system-${ARG_TARGET} ${ARG_QEMU_FLAGS}
             # 等待 gdb 连接
             -S
             # 使用 1234 端口
-            -gdb ${QEMU_GDB_PORT}
-    )
-endfunction()
+            -gdb ${QEMU_GDB_PORT})
+ENDFUNCTION()
