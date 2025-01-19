f1e9e2e8600fa68abd83b4fd2effd875f637217f
Zone.N <zone.niuzh@hotmail.com>
2025-01-19 09:19:29 +0000

    feat: add fdt header
    Signed-off-by: Zone.N <zone.niuzh@hotmail.com>

--- src/kernel/include/kernel_fdt.hpp
+++ src/kernel/include/kernel_fdt.hpp
@@ -47,7 +47,7 @@ class KernelFdt {
    * @param fdt_addr fdt 地址
    */
   explicit KernelFdt(uint64_t fdt_addr)
-      : fdt_addr_(reinterpret_cast<void *>(fdt_addr)) {
+      : fdt_addr_(reinterpret_cast<FdtHeader *>(fdt_addr)) {
     if (fdt_addr_ == nullptr) {
       klog::Err("Fatal Error: Invalid fdt_addr.\n");
       throw;
@@ -145,7 +145,20 @@ class KernelFdt {
   }
 
  private:
-  void *fdt_addr_;
+  struct FdtHeader {
+    uint32_t magic;
+    uint32_t totalsize;
+    uint32_t off_dt_struct;
+    uint32_t off_dt_strings;
+    uint32_t off_mem_rsvmap;
+    uint32_t version;
+    uint32_t last_comp_version;
+    uint32_t boot_cpuid_phys;
+    uint32_t size_dt_strings;
+    uint32_t size_dt_struct;
+  };
+
+  FdtHeader *fdt_addr_;
 };
 
 #endif /* SIMPLEKERNEL_SRC_KERNEL_INCLUDE_KERNEL_FDT_HPP_ */
