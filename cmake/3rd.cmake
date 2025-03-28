a19845a3623ab9fdac5677d4d0a76a40feed27f5
Zone.N <zone.niuzh@hotmail.com>
2025-03-28 13:24:08 +0800

    feat: riscv64 elf on smp
    Signed-off-by: Zone.N <zone.niuzh@hotmail.com>

--- cmake/3rd.cmake
+++ cmake/3rd.cmake
@@ -153,6 +153,30 @@ IF(${CMAKE_SYSTEM_PROCESSOR} STREQUAL "riscv64")
     ADD_SUBDIRECTORY (3rd/opensbi_interface)
 ENDIF()
 
+# https://github.com/u-boot/u-boot.git
+SET (u-boot_SOURCE_DIR ${CMAKE_SOURCE_DIR}/3rd/u-boot)
+SET (u-boot_BINARY_DIR ${CMAKE_BINARY_DIR}/3rd/u-boot)
+ADD_CUSTOM_TARGET (
+    u-boot
+    COMMENT "build u-boot..."
+    # make 时编译
+    ALL
+    DEPENDS $<$<STREQUAL:${CMAKE_SYSTEM_PROCESSOR},riscv64>:opensbi>
+    WORKING_DIRECTORY ${u-boot_SOURCE_DIR}
+    COMMAND ${CMAKE_COMMAND} -E make_directory ${u-boot_BINARY_DIR}
+    COMMAND
+        make O=${u-boot_BINARY_DIR}
+        $<$<STREQUAL:${CMAKE_SYSTEM_PROCESSOR},aarch64>:qemu_arm64_defconfig>
+        $<$<STREQUAL:${CMAKE_SYSTEM_PROCESSOR},riscv64>:qemu-riscv64_spl_defconfig>
+        $<$<STREQUAL:${CMAKE_SYSTEM_PROCESSOR},x86_64>:qemu-x86_64_defconfig>
+        -j${CMAKE_BUILD_PARALLEL_LEVEL}
+    COMMAND
+        make CROSS_COMPILE=${TOOLCHAIN_PREFIX} O=${u-boot_BINARY_DIR}
+        $<$<STREQUAL:${CMAKE_SYSTEM_PROCESSOR},riscv64>:OPENSBI=${opensbi_BINARY_DIR}/platform/generic/firmware/fw_dynamic.bin>
+        -j${CMAKE_BUILD_PARALLEL_LEVEL})
+SET_DIRECTORY_PROPERTIES (PROPERTIES ADDITIONAL_MAKE_CLEAN_FILES
+                                     ${u-boot_BINARY_DIR})
+
 IF(${CMAKE_SYSTEM_PROCESSOR} STREQUAL "aarch64")
     # https://github.com/OP-TEE/optee_os.git
     SET (optee_os_SOURCE_DIR ${CMAKE_SOURCE_DIR}/3rd/optee/optee_os)
@@ -211,30 +235,6 @@ IF(${CMAKE_SYSTEM_PROCESSOR} STREQUAL "aarch64")
     SET_DIRECTORY_PROPERTIES (PROPERTIES ADDITIONAL_MAKE_CLEAN_FILES
                                          ${arm-trusted-firmware_BINARY_DIR})
 ENDIF()
-
-# https://github.com/u-boot/u-boot.git
-SET (u-boot_SOURCE_DIR ${CMAKE_SOURCE_DIR}/3rd/u-boot)
-SET (u-boot_BINARY_DIR ${CMAKE_BINARY_DIR}/3rd/u-boot)
-ADD_CUSTOM_TARGET (
-    u-boot
-    COMMENT "build u-boot..."
-    # make 时编译
-    ALL
-    DEPENDS $<$<STREQUAL:${CMAKE_SYSTEM_PROCESSOR},riscv64>:opensbi>
-    WORKING_DIRECTORY ${u-boot_SOURCE_DIR}
-    COMMAND ${CMAKE_COMMAND} -E make_directory ${u-boot_BINARY_DIR}
-    COMMAND
-        make O=${u-boot_BINARY_DIR}
-        $<$<STREQUAL:${CMAKE_SYSTEM_PROCESSOR},aarch64>:qemu_arm64_defconfig>
-        $<$<STREQUAL:${CMAKE_SYSTEM_PROCESSOR},riscv64>:qemu-riscv64_spl_defconfig>
-        $<$<STREQUAL:${CMAKE_SYSTEM_PROCESSOR},x86_64>:qemu-x86_64_defconfig>
-        -j${CMAKE_BUILD_PARALLEL_LEVEL}
-    COMMAND
-        make CROSS_COMPILE=${TOOLCHAIN_PREFIX} O=${u-boot_BINARY_DIR}
-        $<$<STREQUAL:${CMAKE_SYSTEM_PROCESSOR},riscv64>:OPENSBI=${opensbi_BINARY_DIR}/platform/generic/firmware/fw_dynamic.bin>
-        -j${CMAKE_BUILD_PARALLEL_LEVEL})
-SET_DIRECTORY_PROPERTIES (PROPERTIES ADDITIONAL_MAKE_CLEAN_FILES
-                                     ${u-boot_BINARY_DIR})
 
 # https://git.kernel.org/pub/scm/utils/dtc/dtc.git
 SET (dtc_SOURCE_DIR ${CMAKE_SOURCE_DIR}/3rd/dtc)
