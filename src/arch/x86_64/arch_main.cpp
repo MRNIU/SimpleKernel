eb043c2b89bb545091ecf7154362101a21157ae9
Niu Zhihong <zhihong@nzhnb.com>
2025-07-31 15:30:44 +0800

    AP PE enabled
    Signed-off-by: Niu Zhihong <zhihong@nzhnb.com>

--- src/arch/x86_64/arch_main.cpp
+++ src/arch/x86_64/arch_main.cpp
@@ -44,8 +44,6 @@ BasicInfo::BasicInfo(int argc, const char **argv) {
 
   core_count = cpu_io::GetCpuTopologyInfo().logical_processors;
 }
-extern "C" void *smp_boot_start[];
-extern "C" void *smp_boot_end[];
 
 auto ArchInit(int argc, const char **argv) -> int {
   if (argc != 1) {
@@ -72,15 +70,34 @@ auto ArchInit(int argc, const char **argv) -> int {
   Singleton<Apic>::GetInstance().PrintInfo();
 
   // 计算 SMP 启动代码大小
-  auto smp_boot_size = reinterpret_cast<size_t>(smp_boot_end) -
-                       reinterpret_cast<size_t>(smp_boot_start);
+  auto smp_boot_size = reinterpret_cast<size_t>(ap_start16_end) -
+                       reinterpret_cast<size_t>(ap_start16);
 
-  klog::Info("SMP boot code: start=%p, end=%p, size=%zu bytes\n",
-             smp_boot_start, smp_boot_end, smp_boot_size);
+  klog::Info("SMP boot code: start=%p, end=%p, size=%zu bytes\n", ap_start16,
+             ap_start16_end, smp_boot_size);
+
+  // 填充参数
+  /// @note 这里直接修改了代码段内存的值
+  auto gdt = cpu_io::Gdtr::Read();
+  auto params16 = reinterpret_cast<sipi_params_16bit_t *>(
+      reinterpret_cast<uint64_t>(sipi_params_16bit));
+  params16->ap_start =
+      static_cast<uint32_t>(reinterpret_cast<uint64_t>(nullptr));
+  params16->segment = gdt.selector;
+  params16->gdt = static_cast<uint32_t>(reinterpret_cast<uint64_t>(gdt.base));
+  // params16->gdt_limit = gdt.limit;
+
+  auto aaa = params16->ap_start;
+  auto bbb = params16->gdt;
+  auto ccc = params16->gdt_limit;
+
+  klog::Info(
+      "SIPI params: params16: 0x%x, ap_start=0x%x, gdt=0x%lx, gdt_limit=0x%x\n",
+      params16, aaa, bbb, ccc);
 
   // 唤醒其它 core - 传入 SMP 启动代码的地址和大小
   size_t started_aps = Singleton<Apic>::GetInstance().StartupAllAps(
-      smp_boot_start, smp_boot_size, kDefaultAPBase);
+      ap_start16, smp_boot_size, kDefaultAPBase);
   klog::Info("Started %zu Application Processors\n", started_aps);
 
   return 0;
--- src/arch/x86_64/sipi.h
+++ src/arch/x86_64/sipi.h
@@ -14,4 +14,34 @@
 /// 启动 APs 的默认地址
 static constexpr uint64_t kDefaultAPBase = 0x30000;
 
+extern "C" void *ap_start16[];
+extern "C" void *ap_start16_end[];
+extern "C" void *ap_start[];
+extern "C" void *sipi_params_16bit[];
+extern "C" void *sipi_params[];
+
+/**
+ * 与 boot.S 中的 sipi_params_16bit 结构对应，传递给 ap_start16 的参数
+ */
+struct sipi_params_16bit_t {
+  uint32_t ap_start;
+  uint16_t segment;
+  uint16_t pad;
+  uint16_t gdt_limit;
+  uint32_t gdt;
+  uint16_t unused;
+} __attribute__((packed));
+
+// struct sipi_params {
+//   uint32_t idt_ptr;
+//   uint32_t stack_top;
+//   uint32_t stack_size;
+//   uint32_t microcode_lock;
+//   uint32_t microcode_ptr;
+//   uint32_t msr_table_ptr;
+//   uint32_t msr_count;
+//   uint32_t c_handler;
+//   atomic_t ap_count;
+// };
+
 #endif /* SIMPLEKERNEL_SRC_ARCH_X86_64_SIPI_H_ */
