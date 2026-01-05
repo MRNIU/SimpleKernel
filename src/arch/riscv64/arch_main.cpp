4d6df5090bdcc5f281bba79a8038ec9786764d91
Niu Zhihong <zhihong@nzhnb.com>
2026-01-05 17:50:38 +0800

    style: update
    Signed-off-by: Niu Zhihong <zhihong@nzhnb.com>

--- src/arch/riscv64/arch_main.cpp
+++ src/arch/riscv64/arch_main.cpp
@@ -5,8 +5,6 @@
 #include <cpu_io.h>
 #include <opensbi_interface.h>
 
-#include <bmalloc.hpp>
-
 #include "basic_info.hpp"
 #include "config.h"
 #include "kernel_elf.hpp"
@@ -16,22 +14,6 @@
 #include "per_cpu.hpp"
 #include "sk_cstdio"
 #include "sk_libc.h"
-
-namespace {
-
-struct BmallocLogger {
-  int operator()(const char* format, ...) const {
-    va_list args;
-    va_start(args, format);
-    char buffer[1024];
-    int result = sk_vsnprintf(buffer, sizeof(buffer), format, args);
-    va_end(args);
-    klog::Info("%s", buffer);
-    return result;
-  }
-};
-
-}  // namespace
 
 // 基本输出实现
 extern "C" void sk_putchar(int c, [[maybe_unused]] void* ctx) {
@@ -65,41 +47,6 @@ void ArchInit(int argc, const char** argv) {
   // 解析内核 elf 信息
   Singleton<KernelElf>::GetInstance() =
       KernelElf(Singleton<BasicInfo>::GetInstance().elf_addr);
-
-  void* allocator_addr = reinterpret_cast<void*>(
-      (reinterpret_cast<uintptr_t>(end) + 4095) & ~4095);
-  size_t allocator_size =
-      Singleton<BasicInfo>::GetInstance().physical_memory_size -
-      reinterpret_cast<uintptr_t>(allocator_addr) +
-      Singleton<BasicInfo>::GetInstance().physical_memory_addr;
-
-  klog::Info("bmalloc address: %p\n", allocator_addr);
-  klog::Info("bmalloc size: %zu\n", allocator_size);
-
-  bmalloc::Bmalloc<BmallocLogger> allocator(allocator_addr, allocator_size);
-
-  std::array<size_t, 13> sizes = {1,   2,   4,   8,    16,   32,  64,
-                                  128, 256, 512, 1024, 2048, 4096};
-
-  for (size_t size : sizes) {
-    void* ptr = allocator.malloc(size);
-    if (ptr != nullptr) {
-      // 写入边界位置
-      char* bytes = static_cast<char*>(ptr);
-      bytes[0] = 0xAA;
-      bytes[size - 1] = 0xBB;
-
-      // 验证写入
-      if (memcmp(&bytes[0], "\xAA", 1) != 0) {
-        klog::Err("Memory verification failed for size %zu at start\n", size);
-      }
-      if (memcmp(&bytes[size - 1], "\xBB", 1) != 0) {
-        klog::Err("Memory verification failed for size %zu at end\n", size);
-      }
-
-      allocator.free(ptr);
-    }
-  }
 
   klog::Info("Hello riscv64 ArchInit\n");
 }
