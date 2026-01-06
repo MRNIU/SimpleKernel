e2bfa11c5855ceb06f1f1bc78a53295aa8a5c3b9
Niu Zhihong <zhihong@nzhnb.com>
2026-01-06 16:46:43 +0800

    feat: Add early console support for aarch64, riscv64, and x86_64 architectures
    Signed-off-by: Niu Zhihong <zhihong@nzhnb.com>

---
+++ src/arch/aarch64/early_console.cpp
@@ -0,0 +1,31 @@
+/**
+ * @copyright Copyright The SimpleKernel Contributors
+ */
+
+#include "config.h"
+#include "pl011.h"
+#include "singleton.hpp"
+#include "sk_cstdio"
+
+namespace {
+
+Pl011* pl011 = nullptr;
+
+void console_putchar(int c, [[maybe_unused]] void* ctx) {
+  if (pl011) {
+    pl011->PutChar(c);
+  }
+}
+
+struct EarlyConsole {
+  EarlyConsole() {
+    Singleton<Pl011>::GetInstance() = Pl011(kSimpleKernelEarlyConsoleBase);
+    pl011 = &Singleton<Pl011>::GetInstance();
+
+    sk_putchar = console_putchar;
+  }
+};
+
+EarlyConsole early_console;
+
+}  // namespace
