c527b19970cfbe2a611b337fe3222c05138a69da
Niu Zhihong <zhihong@nzhnb.com>
2026-01-19 12:26:31 +0800

    refactor: timer
    Signed-off-by: Niu Zhihong <zhihong@nzhnb.com>

---
+++ src/arch/x86_64/timer.cpp
@@ -0,0 +1,16 @@
+/**
+ * @copyright Copyright The SimpleKernel Contributors
+ */
+
+#include <cpu_io.h>
+
+#include "arch.h"
+#include "basic_info.hpp"
+#include "interrupt.h"
+#include "opensbi_interface.h"
+#include "singleton.hpp"
+#include "task_manager.hpp"
+
+void TimerInitSMP() {}
+
+void TimerInit() {}
