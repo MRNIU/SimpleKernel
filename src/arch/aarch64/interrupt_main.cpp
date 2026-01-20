6b5af5f451ba73522d48ba3ec3832fb0efb0f58e
Niu Zhihong <zhihong@nzhnb.com>
2026-01-20 12:13:36 +0800

    refactor: timer
    Signed-off-by: Niu Zhihong <zhihong@nzhnb.com>

--- src/arch/aarch64/interrupt_main.cpp
+++ src/arch/aarch64/interrupt_main.cpp
@@ -1,8 +1,8 @@
 /**
  * @copyright Copyright The SimpleKernel Contributors
- * @brief 中断初始化
  */
 
+#include "arch.h"
 #include "basic_info.hpp"
 #include "interrupt.h"
 #include "io.hpp"
@@ -122,38 +122,22 @@ extern "C" void error_lower_el_aarch32_handler(uint64_t sp [[maybe_unused]]) {
   }
 }
 
-auto timer_handler(uint64_t cause, uint8_t *) -> uint64_t {
-  // 2s
-  uint64_t interval_clk = 2 * cpu_io::CNTFRQ_EL0::Read();
-  cpu_io::CNTV_TVAL_EL0::Write(interval_clk);
-  return cause;
-}
-
-auto uart_handler(uint64_t cause, uint8_t *) -> uint64_t {
+auto uart_handler(uint64_t cause, uint8_t*) -> uint64_t {
   sk_putchar(Singleton<Pl011>::GetInstance().TryGetChar(), nullptr);
   return cause;
 }
 
-void InterruptInit(int, const char **) {
+void InterruptInit(int, const char**) {
   cpu_io::VBAR_EL1::Write(reinterpret_cast<uint64_t>(vector_table));
-
-  auto timer_intid =
-      Singleton<KernelFdt>::GetInstance().GetAarch64Intid("arm,armv8-timer") +
-      Gic::kPPIBase;
 
   auto uart_intid =
       Singleton<KernelFdt>::GetInstance().GetAarch64Intid("arm,pl011") +
       Gic::kSPIBase;
 
-  klog::Info("timer_intid: %d, uart_intid: %d\n", timer_intid, uart_intid);
+  klog::Info("uart_intid: %d\n", uart_intid);
 
   Singleton<Interrupt>::GetInstance().SPI(uart_intid,
                                           cpu_io::GetCurrentCoreId());
-  Singleton<Interrupt>::GetInstance().PPI(timer_intid,
-                                          cpu_io::GetCurrentCoreId());
-
-  Singleton<Interrupt>::GetInstance().RegisterInterruptFunc(timer_intid,
-                                                            timer_handler);
 
   // 注册 uart 中断处理函数
   Singleton<Interrupt>::GetInstance().RegisterInterruptFunc(uart_intid,
@@ -161,51 +145,20 @@ void InterruptInit(int, const char **) {
 
   cpu_io::EnableInterrupt();
 
-  cpu_io::CNTV_CTL_EL0::ENABLE::Clear();
-  cpu_io::CNTV_CTL_EL0::IMASK::Set();
-
-  uint64_t interval_clk = 2 * cpu_io::CNTFRQ_EL0::Read();
-  cpu_io::CNTV_TVAL_EL0::Write(interval_clk);
-
-  cpu_io::CNTV_CTL_EL0::ENABLE::Set();
-  cpu_io::CNTV_CTL_EL0::IMASK::Clear();
-
-  // 唤醒其余 core
-  for (size_t i = 0; i < Singleton<BasicInfo>::GetInstance().core_count; i++) {
-    auto ret = cpu_io::psci::CpuOn(i, reinterpret_cast<uint64_t>(_boot), 0);
-    if ((ret != cpu_io::psci::SUCCESS) && (ret != cpu_io::psci::ALREADY_ON)) {
-      klog::Warn("hart %d start failed: %d\n", i, ret);
-    }
-  }
+  // 初始化定时器
+  TimerInit();
 
   klog::Info("Hello InterruptInit\n");
 }
 
-void InterruptInitSMP(int, const char **) {
+void InterruptInitSMP(int, const char**) {
   cpu_io::VBAR_EL1::Write(reinterpret_cast<uint64_t>(vector_table));
 
   Singleton<Interrupt>::GetInstance().SetUP();
 
-  auto timer_intid =
-      Singleton<KernelFdt>::GetInstance().GetAarch64Intid("arm,armv8-timer") +
-      Gic::kPPIBase;
-
-  Singleton<Interrupt>::GetInstance().PPI(timer_intid,
-                                          cpu_io::GetCurrentCoreId());
-
-  Singleton<Interrupt>::GetInstance().RegisterInterruptFunc(timer_intid,
-                                                            timer_handler);
+  TimerInitSMP();
 
   cpu_io::EnableInterrupt();
-
-  cpu_io::CNTV_CTL_EL0::ENABLE::Clear();
-  cpu_io::CNTV_CTL_EL0::IMASK::Set();
-
-  uint64_t interval_clk = 2 * cpu_io::CNTFRQ_EL0::Read();
-  cpu_io::CNTV_TVAL_EL0::Write(interval_clk);
-
-  cpu_io::CNTV_CTL_EL0::ENABLE::Set();
-  cpu_io::CNTV_CTL_EL0::IMASK::Clear();
 
   klog::Info("Hello InterruptInitSMP\n");
 }
--- src/arch/aarch64/timer.cpp
+++ src/arch/aarch64/timer.cpp
@@ -7,8 +7,59 @@
 #include "arch.h"
 #include "basic_info.hpp"
 #include "interrupt.h"
+#include "kernel_fdt.hpp"
 #include "singleton.hpp"
 
-void TimerInitSMP() {}
+namespace {
+uint64_t interval = 0;
+uint64_t timer_intid = 0;
 
-void TimerInit() {}
+auto timer_handler(uint64_t cause, uint8_t*) -> uint64_t {
+  // 2s
+  uint64_t interval_clk = 2 * cpu_io::CNTFRQ_EL0::Read();
+  cpu_io::CNTV_TVAL_EL0::Write(interval_clk);
+  klog::Info("Timer interrupt on core %d\n", cpu_io::GetCurrentCoreId());
+  return cause;
+}
+
+}  // namespace
+
+void TimerInitSMP() {
+  Singleton<Interrupt>::GetInstance().PPI(timer_intid,
+                                          cpu_io::GetCurrentCoreId());
+
+  Singleton<Interrupt>::GetInstance().RegisterInterruptFunc(timer_intid,
+                                                            timer_handler);
+
+  cpu_io::CNTV_CTL_EL0::ENABLE::Clear();
+  cpu_io::CNTV_CTL_EL0::IMASK::Set();
+
+  uint64_t interval_clk = 2 * cpu_io::CNTFRQ_EL0::Read();
+  cpu_io::CNTV_TVAL_EL0::Write(interval_clk);
+
+  cpu_io::CNTV_CTL_EL0::ENABLE::Set();
+  cpu_io::CNTV_CTL_EL0::IMASK::Clear();
+}
+
+void TimerInit() {
+  timer_intid =
+      Singleton<KernelFdt>::GetInstance().GetAarch64Intid("arm,armv8-timer") +
+      Gic::kPPIBase;
+
+  klog::Info("timer_intid: %d\n", timer_intid);
+
+  Singleton<Interrupt>::GetInstance().PPI(timer_intid,
+                                          cpu_io::GetCurrentCoreId());
+
+  Singleton<Interrupt>::GetInstance().RegisterInterruptFunc(timer_intid,
+                                                            timer_handler);
+
+  cpu_io::CNTV_CTL_EL0::ENABLE::Clear();
+  cpu_io::CNTV_CTL_EL0::IMASK::Set();
+
+  uint64_t interval_clk = 2 * cpu_io::CNTFRQ_EL0::Read();
+  cpu_io::CNTV_TVAL_EL0::Write(interval_clk);
+
+  cpu_io::CNTV_CTL_EL0::ENABLE::Set();
+  cpu_io::CNTV_CTL_EL0::IMASK::Clear();
+}
