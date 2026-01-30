df8b9da371b52f660d09e6ef4dcfee717f6556b1
Niu Zhihong <zhihong@nzhnb.com>
2026-01-30 17:37:41 +0800

    feat: assert
    Signed-off-by: Niu Zhihong <zhihong@nzhnb.com>

--- tests/unit_test/kernel_fdt_test.cpp
+++ tests/unit_test/kernel_fdt_test.cpp
@@ -16,8 +16,10 @@ TEST(KernelFdtTest, ConstructorTest) {
 TEST(KernelFdtTest, GetMemoryTest) {
   KernelFdt kerlen_fdt((uint64_t)riscv64_virt_dtb_data);
 
-  auto [memory_base, memory_size] = kerlen_fdt.GetMemory();
+  auto result = kerlen_fdt.GetMemory();
+  ASSERT_TRUE(result.has_value()) << result.error().message();
 
+  auto [memory_base, memory_size] = *result;
   EXPECT_EQ(memory_base, 0x80000000);
   EXPECT_EQ(memory_size, 0x8000000);
 }
@@ -25,8 +27,10 @@ TEST(KernelFdtTest, GetMemoryTest) {
 TEST(KernelFdtTest, GetSerialTest) {
   KernelFdt kerlen_fdt((uint64_t)riscv64_virt_dtb_data);
 
-  auto [serial_base, serial_size, serial_irq] = kerlen_fdt.GetSerial();
+  auto result = kerlen_fdt.GetSerial();
+  ASSERT_TRUE(result.has_value()) << result.error().message();
 
+  auto [serial_base, serial_size, serial_irq] = *result;
   EXPECT_EQ(serial_base, 0x10000000);
   EXPECT_EQ(serial_size, 0x100);
   EXPECT_EQ(serial_irq, 0xA);
@@ -35,25 +39,33 @@ TEST(KernelFdtTest, GetSerialTest) {
 TEST(KernelFdtTest, GetTimebaseFrequencyTest) {
   KernelFdt kerlen_fdt((uint64_t)riscv64_virt_dtb_data);
 
-  auto time_base_frequency = kerlen_fdt.GetTimebaseFrequency();
+  auto result = kerlen_fdt.GetTimebaseFrequency();
+  ASSERT_TRUE(result.has_value()) << result.error().message();
 
-  EXPECT_EQ(time_base_frequency, 0x989680);
+  EXPECT_EQ(*result, 0x989680);
 }
 
 TEST(KernelFdtTest, GetCoreCountTest) {
   KernelFdt kerlen_fdt((uint64_t)riscv64_virt_dtb_data);
 
-  auto core_count = kerlen_fdt.GetCoreCount();
+  auto result = kerlen_fdt.GetCoreCount();
+  ASSERT_TRUE(result.has_value()) << result.error().message();
 
-  EXPECT_GT(core_count, 0);  // 至少有一个 CPU 核心
+  EXPECT_GT(*result, 0);  // 至少有一个 CPU 核心
 }
 
 TEST(KernelFdtTest, CopyConstructorTest) {
   KernelFdt kerlen_fdt((uint64_t)riscv64_virt_dtb_data);
   KernelFdt kerlen_fdt2(kerlen_fdt);
 
-  auto [memory_base1, memory_size1] = kerlen_fdt.GetMemory();
-  auto [memory_base2, memory_size2] = kerlen_fdt2.GetMemory();
+  auto result1 = kerlen_fdt.GetMemory();
+  auto result2 = kerlen_fdt2.GetMemory();
+
+  ASSERT_TRUE(result1.has_value());
+  ASSERT_TRUE(result2.has_value());
+
+  auto [memory_base1, memory_size1] = *result1;
+  auto [memory_base2, memory_size2] = *result2;
 
   EXPECT_EQ(memory_base1, memory_base2);
   EXPECT_EQ(memory_size1, memory_size2);
@@ -65,8 +77,14 @@ TEST(KernelFdtTest, AssignmentTest) {
 
   kerlen_fdt2 = kerlen_fdt;
 
-  auto [memory_base1, memory_size1] = kerlen_fdt.GetMemory();
-  auto [memory_base2, memory_size2] = kerlen_fdt2.GetMemory();
+  auto result1 = kerlen_fdt.GetMemory();
+  auto result2 = kerlen_fdt2.GetMemory();
+
+  ASSERT_TRUE(result1.has_value());
+  ASSERT_TRUE(result2.has_value());
+
+  auto [memory_base1, memory_size1] = *result1;
+  auto [memory_base2, memory_size2] = *result2;
 
   EXPECT_EQ(memory_base1, memory_base2);
   EXPECT_EQ(memory_size1, memory_size2);
@@ -74,23 +92,33 @@ TEST(KernelFdtTest, AssignmentTest) {
 
 TEST(KernelFdtTest, MoveConstructorTest) {
   KernelFdt kerlen_fdt((uint64_t)riscv64_virt_dtb_data);
-  auto [expected_base, expected_size] = kerlen_fdt.GetMemory();
+  auto result = kerlen_fdt.GetMemory();
+  ASSERT_TRUE(result.has_value());
+  auto [expected_base, expected_size] = *result;
 
   KernelFdt kerlen_fdt2(std::move(kerlen_fdt));
 
-  auto [memory_base, memory_size] = kerlen_fdt2.GetMemory();
+  auto result2 = kerlen_fdt2.GetMemory();
+  ASSERT_TRUE(result2.has_value());
+  auto [memory_base, memory_size] = *result2;
+
   EXPECT_EQ(memory_base, expected_base);
   EXPECT_EQ(memory_size, expected_size);
 }
 
 TEST(KernelFdtTest, MoveAssignmentTest) {
   KernelFdt kerlen_fdt((uint64_t)riscv64_virt_dtb_data);
-  auto [expected_base, expected_size] = kerlen_fdt.GetMemory();
+  auto result = kerlen_fdt.GetMemory();
+  ASSERT_TRUE(result.has_value());
+  auto [expected_base, expected_size] = *result;
 
   KernelFdt kerlen_fdt2;
   kerlen_fdt2 = std::move(kerlen_fdt);
 
-  auto [memory_base, memory_size] = kerlen_fdt2.GetMemory();
+  auto result2 = kerlen_fdt2.GetMemory();
+  ASSERT_TRUE(result2.has_value());
+  auto [memory_base, memory_size] = *result2;
+
   EXPECT_EQ(memory_base, expected_base);
   EXPECT_EQ(memory_size, expected_size);
 }
