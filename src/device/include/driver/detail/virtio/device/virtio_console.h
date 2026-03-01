fb0b357bddc9419056379a7c8c60910aae2c513b
Niu Zhihong <zhihong@nzhnb.com>
2026-03-01 13:01:32 +0800

    feat(device): add detail/virtio/device/ inlined from device_framework
    Signed-off-by: Niu Zhihong <zhihong@nzhnb.com>

---
+++ src/device/include/driver/detail/virtio/device/virtio_console.h
@@ -0,0 +1,10 @@
+/**
+ * @copyright Copyright The SimpleKernel Contributors
+ */
+
+#ifndef SIMPLEKERNEL_SRC_DEVICE_INCLUDE_DRIVER_DETAIL_VIRTIO_DEVICE_VIRTIO_CONSOLE_H_
+#define SIMPLEKERNEL_SRC_DEVICE_INCLUDE_DRIVER_DETAIL_VIRTIO_DEVICE_VIRTIO_CONSOLE_H_
+
+#endif /*                                                                     \
+SIMPLEKERNEL_SRC_DEVICE_INCLUDE_DRIVER_DETAIL_VIRTIO_DEVICE_VIRTIO_CONSOLE_H_ \
+        */
